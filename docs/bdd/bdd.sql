cd27d958881a9209eae42680eb8a2dd58450a5dd
Anne Wilga <anne.wilga@orange.fr>
2021-04-29 10:12:27 +0200

    remis en état

--- docs/bdd/bdd.sql
+++ docs/bdd/bdd.sql
@@ -9,18 +9,16 @@ USE wazaaImmoGroup;
 CREATE TABLE waz_biens
 (
    bi_id INT(10) NOT NULL AUTO_INCREMENT COMMENT 'Identifiant / Clé primaire',
-   bi_type VARCHAR(25) NOT NULL COMMENT 'Type de bien',
+   bi_type VARCHAR(25) NOT NULL CHECK (bi_type IN('maison','garage','terrain','locaux_professionnels','immeuble','bureaux','appartement')) COMMENT 'Type de bien',
    bi_pieces char(4) NOT NULL CHECK (bi_pieces IN ('1','2','3','4','5','6','+6','NULL'))COMMENT 'Nombre de pièces' ,
-   -- substring = extraict une partie de la chaine
-   bi_ref CHAR(11) NOT NULL  CHECK (SUBSTRING(bi_ref, 1, 1) <> ' ')   COMMENT 'Référence de l''annonce',
-   bi_description TEXT NOT NULL,
-   bi_local VARCHAR(100) NOT NULL,
+   bi_ref CHAR(11) NOT NULL  CHECK (REGEXP_LIKE(bi_ref,'^(A{1}P{1}|M{1}A{1}|T{1}E{1}|G{1}A{1}|I{1}M{1}|L{1}P{1}|B{1}U{1})[-]([0-9]{4})[-]([0-9]{2})$')=1)  COMMENT 'Référence du bien',
+   bi_description TEXT NOT NULL CHECK (REGEXP_LIKE(bi_description,'^[^ ]')),
+   bi_local VARCHAR(100) NOT NULL CHECK (REGEXP_LIKE(bi_local,'^[^ ]')),
    bi_surf_habitable INT (11) NOT NULL CHECK (bi_surf_habitable > 0) COMMENT 'Surface habitable (mètres carrés)',
    bi_surf_totale INT (11) NOT NULL CHECK (bi_surf_totale > 0) COMMENT 'Surface totale/terrain (mètres carrés)',
    bi_estimations_vente DECIMAL(8,2) NOT NULL CHECK (bi_estimations_vente > 0) ,
    bi_estimation_location DECIMAL(8,2) NOT NULL CHECK (bi_estimation_location > 0) ,
-   bi_diagnostic CHAR(1) NOT NULL CHECK (bi_diagnostic IN ('A','B','C','D','E','F','G','V')) 
-   COMMENT 'Lettre du diagnostic : A à G + V pour vierge ',
+   bi_diagnostic CHAR(1) NOT NULL CHECK (bi_diagnostic IN ('A','B','C','D','E','F','G','V')) COMMENT 'Lettre du diagnostic : A à G + V pour vierge ',
    PRIMARY KEY(bi_id),
    INDEX ind_biens_6(bi_type,bi_pieces,bi_ref,bi_local,bi_surf_habitable,bi_surf_totale),
    INDEX ind_biens_4(bi_pieces,bi_ref,bi_local,bi_surf_habitable),
@@ -58,7 +56,7 @@ INSERT INTO waz_options (opt_id, opt_libelle) VALUES
 CREATE TABLE waz_photos
 (
    pho_id INT(10) NOT NULL AUTO_INCREMENT,
-   pho_nom VARCHAR(30) NOT NULL,
+   pho_nom VARCHAR(30) NOT NULL CHECK(REGEXP_LIKE(pho_nom,'^[^ ]')),
    bi_id INT(10) NOT NULL,
    PRIMARY KEY(pho_id),
    FOREIGN KEY(bi_id) REFERENCES waz_biens(bi_id)
@@ -69,33 +67,25 @@ CREATE TABLE waz_photos
 CREATE TABLE waz_employes
 (
    emp_id INT(10) NOT NULL AUTO_INCREMENT,
-   emp_nom VARCHAR(50) NOT NULL CHECK (COALESCE(SUBSTRING(emp_nom, 1, 1), 'X') 
-      BETWEEN 'A' AND 'Z' AND CHARACTER_LENGTH(emp_nom) > 2),-- accepte a partir de 3 caracteres 
-   -- colaesce -> segmente la chaine caractere 
-   -- substring -> renvoie la première valeur non nulle d'une liste.
-   emp_prenom VARCHAR(50) NOT NULL CHECK (COALESCE(SUBSTRING(emp_prenom, 1, 1), 'X') 
-      BETWEEN 'A' AND 'Z'AND CHARACTER_LENGTH(emp_prenom) > 2),
-   emp_adresse VARCHAR(50) NOT NULL,
-   emp_tel VARCHAR(50) NOT NULL ,
-   emp_mail VARCHAR(50) NOT NULL,
-   emp_poste VARCHAR(50) NOT NULL,
-   emp_mdp VARCHAR(50) NOT NULL,
-   PRIMARY KEY(emp_id),
-   UNIQUE(emp_mail)
+   emp_nom VARCHAR(50) NOT NULL CHECK (REGEXP_LIKE(emp_nom,'(^[^ \\W\\d_-])([A-Za-zàáâäçèéêëìíîïñòóôöùúûüwW ]+)([ -]?)[A-Za-zàáâäçèéêëìíîïñòóôöùúûü]+$')),
+   emp_prenom VARCHAR(50) NOT NULL CHECK (REGEXP_LIKE(emp_prenom,'(^[^ \\W\\d_-])([A-Za-zàáâäçèéêëìíîïñòóôöùúûüwW ]+)([ -]?)[A-Za-zàáâäçèéêëìíîïñòóôöùúûü]+$')),
+   emp_adresse VARCHAR(50) NOT NULL CHECK (REGEXP_LIKE(emp_adresse,"^[^ \s][a-zA-Z ,\\d\/\\\*.%&'-]{3,}$")),
+   emp_tel VARCHAR(50) NOT NULL CHECK (REGEXP_LIKE(emp_tel,"^[^ \W]([0-9 \/\-])*$")),
+   emp_mail VARCHAR(50) NOT NULL UNIQUE CHECK (REGEXP_LIKE(emp_mail,'([a-z0-9.-])+@([a-z0-9.-]{2,})([.]{1})[a-z.-]{2,4}$')),
+   emp_poste VARCHAR(50) NOT NULL CHECK (REGEXP_LIKE(emp_poste,'^[a-zA-Z ]{3,}$')),
+   emp_mdp VARCHAR(50) NOT NULL CHECK(REGEXP_LIKE(emp_mdp,'^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[+!*$@%_-])([+!*$@%_\\w -]{8,15})$')),
+   PRIMARY KEY(emp_id)
 );
-
 -- Structure de la table waz_utilisateurs
 
 CREATE TABLE waz_internautes
 (
    in_id INT(10) NOT NULL AUTO_INCREMENT,
-   in_nom VARCHAR(30) NOT NULL CHECK(COALESCE(SUBSTRING(in_nom, 1, 1), 'X') 
-      BETWEEN 'A' AND 'Z' AND CHARACTER_LENGTH(in_nom) > 2 ),
-   in_prenom VARCHAR(30) NOT NULL CHECK(COALESCE(SUBSTRING(in_prenom, 1, 1), 'X') 
-      BETWEEN 'A' AND 'Z' AND CHARACTER_LENGTH(in_prenom) > 2 ),
+   in_nom VARCHAR(30) NOT NULL CHECK (REGEXP_LIKE(in_nom,'(^[^ \\W\\d_-])([A-Za-zàáâäçèéêëìíîïñòóôöùúûü ]+)([ -]?)[A-Za-zàáâäçèéêëìíîïñòóôöùúûü]+$')),
+   in_prenom VARCHAR(30) NOT NULL CHECK(REGEXP_LIKE(in_prenom,'(^[^ \\W\\d_-])([A-Za-zàáâäçèéêëìíîïñòóôöùúûü ]+)([ -]?)[A-Za-zàáâäçèéêëìíîïñòóôöùúûü]+$')),
    in_adresse VARCHAR(50) NOT NULL,
-   in_telephone VARCHAR(50) NOT NULL,
-   in_email VARCHAR(50) NOT NULL  ,
+   in_telephone VARCHAR(50) NOT NULL CHECK (REGEXP_LIKE (in_telephone,'^[^ \W]([0-9 \/\-])*$')),
+   in_email VARCHAR(50) NOT NULL  CHECK (REGEXP_LIKE(in_email,'([a-z0-9.-])+@([a-z0-9.-]{2,})([.]{1})[a-z]{2,4}$')),
    in_pays VARCHAR(50) NOT NULL,
    in_est_contacter BOOLEAN NOT NULL DEFAULT 0 CHECK (in_est_contacter IN ('0', '1') ) 
    COMMENT '1=contacter 0=non contacter', 
@@ -113,12 +103,12 @@ CREATE TABLE waz_annonces
    an_id INT(10) NOT NULL AUTO_INCREMENT,
    an_prix DECIMAL(8,2) NOT NULL CHECK (an_prix>=0) COMMENT 'Prix en euros',
    an_est_active BOOLEAN NOT NULL CHECK (an_est_active IN ('0','1')) COMMENT '1=active 0=non active',
-   an_ref CHAR(20) NOT NULL COMMENT 'Référence de l''annonce',
+   an_ref CHAR(20) NOT NULL CHECK (REGEXP_LIKE(an_ref,'^((V{1}E{1})|(L{1}O{1})|(A{1}C{1}))[-](A{1}P{1}|M{1}A{1}|T{1}E{1}|G{1}A{1}|I{1}M{1}|L{1}P{1}|B{1}U{1})[-]([0-9]{4})[-]([0-9]{2})$')=1)  COMMENT 'Référence de l''annonce',
    an_date_disponibilite DATE NOT NULL,
    an_offre CHAR(1) NOT NULL CHECK (an_offre IN ('A','L','V')) COMMENT 'Type d''offre. Lettres A, L ou V.',
    an_nbre_vues SMALLINT(6) NOT NULL CHECK (an_nbre_vues >=0),
    an_date_ajout DATE NOT NULL DEFAULT (CURRENT_DATE),
-   an_date_modif DATETIME NOT NULL DEFAULT (CURRENT_DATE),
+   an_date_modif DATETIME  DEFAULT NULL ,
    an_titre VARCHAR(255) NOT NULL,
    bi_id INT(10) NOT NULL,
    PRIMARY KEY(an_id),
@@ -176,7 +166,7 @@ CREATE TABLE waz_contacter
 (
    emp_id INT(10),
    in_id INT(10),
-   co_sujet VARCHAR(50) NOT NULL,
+   co_sujet VARCHAR(50) NOT NULL CHECK(co_sujet IN ('acheter','vendre','louer','autres')),
    co_question TEXT NOT NULL,
    PRIMARY KEY(emp_id, in_id),
    FOREIGN KEY(emp_id) REFERENCES waz_employes(emp_id),
--- docs/bdd/trigger.sql
+++ docs/bdd/trigger.sql
@@ -79,12 +79,15 @@ END |
 DELIMITER ;
 
 -- exemple d'insertion
--- INSERT INTO `waz_negocier` (`emp_id`, `in_id`, `an_id`, `neg_est_conclu`, `neg_montant_transaction`, `neg_date_debut_transaction`, `neg_date_transaction_fin`, `neg_date_dernier_contact`)VALUES
+-- INSERT INTO `waz_negocier` (`emp_id`, `in_id`, `an_id`, `neg_est_conclu`, `neg_montant_transaction`,
+--  `neg_date_debut_transaction`, `neg_date_transaction_fin`, `neg_date_dernier_contact`)VALUES
 -- (8, 4, 1, 0, '120.00', '2021-01-27', '2021-03-17', '2021-04-17');
 
-
--- trigger pour la table hist_negocier 
+-- ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+-- trigger pour historisation de  la table waz_negocier ,quand une valeur est effacer , 
+-- elle sera sauvegarder dans la table hist_negocier.
 DELIMITER |
+DROP TRIGGER if EXISTS after_update_negocier |
 CREATE TRIGGER after_update_negocier 
 AFTER UPDATE 
 ON waz_negocier
@@ -125,6 +128,7 @@ END|
 CREATE TRIGGER after_delete_negocier 
 AFTER DELETE
 ON waz_negocier
+FOR EACH ROW
 BEGIN
     INSERT INTO histo_negocier
     (
@@ -157,8 +161,43 @@ BEGIN
       'DELETE'
    )
 END|
-DELIMITER ;
 
+DELIMITER ;
+-- ******************************************************************************************************************
+-- trigger pour apres la modification ou l'insertion pour la table waz_annonce ,si la regex du champ != de la regex ,
+-- alors il aura un message d'erreur .
+
+DELIMITER |
+DROP TRIGGER if EXISTS before_update_annonces |
+CREATE TRIGGER before_update_annonces
+BEFORE UPDATE
+ON waz_annonces
+FOR EACH ROW
+BEGIN
+    	IF new.an_ref NOT REGEXP '^((V{1}E{1})|(L{1}O{1})|(A{1}C{1}))[-](A{1}P{1}|M{1}A{1}|T{1}E{1}|G{1}A{1}|I{1}M{1}|
+        L{1}P{1}|B{1}U{1})[-]([0-9]{4})[-]([0-9]{2})$'
+    THEN
+      SIGNAL SQLSTATE '12345'
+     SET MESSAGE_TEXT = 'Erreur de format!!!';
+   
+     END IF;
+END |
+
+DROP TRIGGER if EXISTS before_insert_annonces |
+CREATE TRIGGER before_insert_annonces
+BEFORE INSERT
+ON waz_annonces
+FOR EACH ROW
+BEGIN
+    	IF new.an_ref NOT REGEXP '^((V{1}E{1})|(L{1}O{1})|(A{1}C{1}))[-](A{1}P{1}|M{1}A{1}|T{1}E{1}|G{1}A{1}|I{1}M{1}|
+        L{1}P{1}|B{1}U{1})[-]([0-9]{4})[-]([0-9]{2})$'
+    THEN
+      SIGNAL SQLSTATE '12345'
+     SET MESSAGE_TEXT = 'Erreur de format!!!';
+   
+     END IF;
+END |
+DELIMITER ;
 
 
 
