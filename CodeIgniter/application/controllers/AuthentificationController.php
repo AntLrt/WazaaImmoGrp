f9f0383008e52c6935f40f67cf3f4520f5c37c99
Dos Santos Vincent <vincedos@hotmail.com>
2021-05-05 20:00:57 +0200

    Commit PHP Vincent&Julien
    Modification deu fichier config autoload pour prendre en charge les essions,
    Ajout de la possibilité de se connecter en tant qu'internaute ou employé si les identifiants sont bons,
    Ajout de la possibilié de se déconnecter,
    Seul un internaute connecté peut maintenant laisser un commentaire,
    Seul un employé connecté peut maintenant accéder aux pages admin,
    Page "votre compte" uniquement accessible si connecté sinon redirigé vers la page de connexion,
    Modification d'emplacement de certaines fonctions sur d'autres controllers,
    Modification de certains liens,
    Formulaire de contact accessible uniquement par internaute connecté ou sinon redirigé vers page de connexion,
    ID du formulaire de contact envoyé maintenant correspondant a l'id de la session,
    ID de l'envoi de commentaire maintenant correspondant a l'id de la session.

--- CodeIgniter/application/controllers/AuthentificationController.php
+++ CodeIgniter/application/controllers/AuthentificationController.php
@@ -1,91 +1,164 @@
 <?php
 
+//connexion et inscription + mdp oublié
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class AuthentificationController extends CI_Controller {
 
-        public function ajouter()
-        {
-           // Chargement des assistants 'form' et 'url'
-           $this->load->helper('form', 'url'); 
+    public function login()
+    {
+    //afficher aide au debug
+    $this->output->enable_profiler(TRUE);
     
-           // Chargement de la librairie 'database'
-           $this->load->database(); 
-    
-           // Chargement de la librairie form_validation
-           $this->load->library('form_validation'); 
-    
-            
+    // Chargement des assistants 'form' et 'url'
+    $this->load->helper('form', 'url'); 
 
-           if ($this->input->post()) 
-           { // 2ème appel de la page: traitement du formulaire
+    // Chargement de la librairie 'database'
+    $this->load->database(); 
+
+
+
+    if ($this->input->post()) 
+    { // 2ème appel de la page: traitement du formulaire
+
+    $data = $this->input->post();
+
+    $LoginEmp = $_POST['login_name'];
+    $MdpEmp = $_POST['password_name'];
+
+    $LoginInt = $_POST['login_name'];
+    $MdpInt = $_POST['password_name'];
+
+    // Exécute la requête 
+    $resultsemp = $this->db->query("SELECT emp_nom,emp_prenom,emp_id
+    FROM waz_employes 
+    WHERE emp_mail = '$LoginEmp' AND emp_mdp= '$MdpEmp'");
+
+    // Récupération des résultats
+    $RequeteEmploye = $resultsemp->result();
+
+    foreach ($RequeteEmploye as $row) { $EmpPrenom = $row->emp_prenom; $EmpNom = $row->emp_nom; $EmpID = $row->emp_id; }
+
+    // Exécute la requête 
+    $resultsint = $this->db->query("SELECT in_nom,in_prenom,in_id 
+    FROM waz_internautes 
+    WHERE in_email = '$LoginInt' AND in_mdp= '$MdpInt'");
+
+    // Récupération des résultats
+    $RequeteInternaute = $resultsint->result();
+
+    foreach ($RequeteInternaute as $row) { $IntPrenom = $row->in_prenom; $IntNom = $row->in_nom; $IntID = $row->in_id; }
+
+
+    $EstUnEmploye = false;
+    $EstUninternaute = false;
+
+    if(!empty($RequeteEmploye)){$EstUnEmploye = true;}
+    if(!empty($RequeteInternaute)){$EstUninternaute = true;}
+
+
+    if (empty($RequeteEmploye) && empty($RequeteInternaute)) {
+
+    echo"<script type='text/javascript'>
+    window.alert('Connexion refusé')
+    </script>";
     
-                $data = $this->input->post();
+    $this->load->view('ConnexionView');
+    } 
 
-                
-                 // Définition des filtres, ici une valeur doit avoir été saisie pour le champ 'pro_ref'
-                
-                
-                 $config = array(
-                        array(
-                                'field' => 'in_nom',
-                                'label' => 'Nom',
-                                'rules' => 'required'
-                        )
-                        );
-                
-                
-                
-                $this->form_validation->set_rules($config);
-                $this->form_validation->set_rules("in_prenom", "prénom", "required|max_length[15]", array("required" => "Le %s doit être obligatoire.","max_length" => "Le %s doit avoir une longueur maximum de 15 caractères !"));
-                $this->form_validation->set_rules("in_telephone", "Téléphone", array('required', 'min_length[10]', 'max_length[10]', 'callback_tel_check')); 
-                $this->form_validation->set_rules('in_email', 'Email', 'trim|required|valid_email',); 
-                
-                $config = array(
-                        array(
-                                'field' => 'in_adresse',
-                                'label' => 'Adresse',
-                                'rules' => 'required'
-                        )
-                        );
-               
-                
+    else {
+    //$this-> load->library('session');
 
-                
-                
-                if ($this->form_validation->run() == FALSE)
-                { // Echec de la validation, on réaffiche la vue formulaire 
-        
-                      $this->load->view('ajouter');
-                }
-                else
-                { // La validation a réussi, nos valeurs sont bonnes, on peut insérer en base
-               
-               
-               
-               
-               
-                $this->db->insert('waz_internautes', $data);
+    if($EstUnEmploye == true){
+    $this->session->set_userdata('role', "Employe");
+    $this->session->set_userdata('login', "$LoginEmp");
+    $this->session->set_userdata('nom', "$EmpPrenom");
+    $this->session->set_userdata('prenom', "$EmpNom");
+    $this->session->set_userdata('ID', "$EmpID");
+    }
     
-                $this->load->view('congratz');
-           } 
-        }
-           else 
-           { // 1er appel de la page: affichage du formulaire
-               $this->load->view('ajouter');
-           }
-        } // -- ajouter()
+    if($EstUninternaute == true){
+    $this->session->set_userdata('role', "Internaute");
+    $this->session->set_userdata('login', "$LoginInt");
+    $this->session->set_userdata('nom', "$IntPrenom");
+    $this->session->set_userdata('prenom', "$IntNom");
+    $this->session->set_userdata('ID', "$IntID");
+    }
 
-public function tel_check($str)
+    echo $this->session->login;
+    echo $this->session->role;
+
+    $this->load->view('ConnexionReussi');
+    }
+
+
+
+
+} 
+
+    else 
+    { // 1er appel de la page: affichage du formulaire
+    $this->load->view('ConnexionView');
+}
+}
+
+
+public function Deconnexion()
 {
-        if ($str == '0610531272')
+session_destroy();
+header('Location: http://localhost/ci/index.php/AccueilController/Accueil');      
+exit(); 
+}
+
+
+public function DetailsCompte()
+{
+// $IDcompte = L'itrnasmis par la connexion
+
+
+
+//afficher aide au debug
+$this->output->enable_profiler(TRUE);
+
+// Charge la librairie 'database'
+$this->load->database();
+
+if($this->session->role == "Internaute"){
+    $Login = $this->session->login;
+    // Exécute la requête 
+    $DetailsInternaute = $this->db->query("SELECT * 
+    FROM waz_internautes
+    WHERE in_email='$Login'");  
+
+    $Details = $DetailsInternaute->result();   
+
+    // Ajoute des résultats de la requête au tableau des variables à transmettre à la vue   
+    $aView["Details"] = $Details;
+
+    // Appel de la vue avec transmission du tableau  
+    $this->load->view('DetailsCompteView', $aView);
+    }
+
+else if($this->session->role == "Employe")
         {
-                $this->form_validation->set_message('tel_check', 'Le {field} est déjà enregistrée ! :( ');
-                return FALSE;
+        $Login = $this->session->login;
+        // Exécute la requête 
+        $DetailsEmploye = $this->db->query("SELECT * 
+        FROM waz_employes
+        WHERE emp_mail='$Login'");  
+    
+        $Details = $DetailsEmploye->result();   
+    
+        // Ajoute des résultats de la requête au tableau des variables à transmettre à la vue   
+        $aView["Details"] = $Details;
+    
+        // Appel de la vue avec transmission du tableau  
+        $this->load->view('DetailsCompteView', $aView);
         }
-        else
-        {
-                return TRUE;
-        }
+else{
+        header('Location: http://localhost/ci/index.php/AuthentificationController/login');
+        exit(); 
+    }
 }
 }
