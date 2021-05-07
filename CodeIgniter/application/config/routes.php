b151fa73630d6ec8952efce588dda348469cd135
Dos Santos Vincent <vincedos@hotmail.com>
2021-05-07 12:30:38 +0200

    Màj VirtualHost + port 3306

--- CodeIgniter/application/config/routes.php
+++ CodeIgniter/application/config/routes.php
@@ -49,6 +49,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'produits/liste';
+$route['default_controller'] = 'AccueilController/Accueil';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
