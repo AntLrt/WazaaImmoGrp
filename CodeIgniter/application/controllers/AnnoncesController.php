f6838c0ad1f70760927f7ccd4b869d81081b3923
Dos Santos Vincent <vincedos@hotmail.com>
2021-05-12 17:52:08 +0200

    Commit PHP
    Ajout de la liste des options disponible dans les détails de l'annonce,
    Si pas d'options disponible pour l'annonce, c'est precisé,
    Maintenant lorsque rien n'est trouvé par la barre de recherche, une nouvelle barre de recherche apparait,
    Certains liens rendus automatique,
    Minimum d'un seule requête dans la fonction login,
    Système de vue fait : a chaque visite des détaials d'une annonce l'annonce en question gagne une vue,

--- CodeIgniter/application/controllers/AnnoncesController.php
+++ CodeIgniter/application/controllers/AnnoncesController.php
@@ -2,89 +2,122 @@
 
 //page location et pages ventes + details des annonces
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-
-class AnnoncesController extends CI_Controller 
-
+class AnnoncesController extends CI_Controller
 {
 
+        
 public function Loca()
 {
 //afficher aide au debug
-$this->output->enable_profiler(TRUE);
+        $this->output->enable_profiler(true);
 
 // Chargement de la librairie 'database'
-$this->load->database(); 
+        $this->load->database();
 
-// Exécute la requête 
-$results = $this->db->query("SELECT * 
-FROM waz_annonces,waz_biens,waz_photos 
-WHERE an_offre = 'L' 
-AND waz_annonces.an_id=waz_biens.bi_id 
-AND waz_photos.bi_id=waz_biens.bi_id");  
+// Exécute la requête
+        $results = $this->db->query("SELECT *
+FROM waz_annonces,waz_biens,waz_photos
+WHERE an_offre = 'L'
+AND waz_annonces.an_id=waz_biens.bi_id
+AND waz_photos.bi_id=waz_biens.bi_id");
 
-// Récupération des résultats    
-$aListe = $results->result();   
+// Récupération des résultats
+        $aListe = $results->result();
 
-// Ajoute des résultats de la requête au tableau des variables à transmettre à la vue   
-$aView["locations"] = $aListe;
+// Ajoute des résultats de la requête au tableau des variables à transmettre à la vue
+        $aView["locations"] = $aListe;
 
-$this->load->view('Headerview');
-$this->load->view('LocaView', $aView);
+        $this->load->view('Headerview');
+        $this->load->view('LocaView', $aView);
 
 }
+
+
 
 
 public function Ventes()
 {
 //afficher aide au debug
-$this->output->enable_profiler(TRUE);
+        $this->output->enable_profiler(true);
 
 // Chargement de la librairie 'database'
-$this->load->database(); 
+        $this->load->database();
 
-// Exécute la requête 
-$results = $this->db->query("SELECT * 
-FROM waz_annonces,waz_biens,waz_photos 
-WHERE an_offre = 'A' 
-AND waz_annonces.an_id=waz_biens.bi_id 
-AND waz_photos.bi_id=waz_biens.bi_id");  
+// Exécute la requête
+        $results = $this->db->query("SELECT *
+FROM waz_annonces,waz_biens,waz_photos
+WHERE an_offre = 'A'
+AND waz_annonces.an_id=waz_biens.bi_id
+AND waz_photos.bi_id=waz_biens.bi_id");
 
-// Récupération des résultats    
-$aListe = $results->result();   
+// Récupération des résultats
+        $aListe = $results->result();
 
-// Ajoute des résultats de la requête au tableau des variables à transmettre à la vue   
-$aView["ventes"] = $aListe;
+// Ajoute des résultats de la requête au tableau des variables à transmettre à la vue
+        $aView["ventes"] = $aListe;
 
-$this->load->view('Headerview');
-$this->load->view('VentesView', $aView);
+        $this->load->view('Headerview');
+        $this->load->view('VentesView', $aView);
 
 }
+
+
 
 
 public function Details($an_id)
 {
 //afficher aide au debug
-$this->output->enable_profiler(TRUE);
+        $this->output->enable_profiler(true);
 
 // Chargement de la librairie 'database'
-$this->load->database(); 
+        $this->load->database();
 
-// Exécute la requête 
-$results = $this->db->query("SELECT * 
+// Exécute la requête
+        $Resultat1 = $this->db->query("SELECT *
 FROM waz_annonces,waz_biens,waz_photos
-WHERE an_id = '$an_id' AND waz_annonces.an_id=waz_biens.bi_id AND waz_photos.bi_id=waz_biens.bi_id");  
+WHERE an_id = '$an_id' AND waz_annonces.an_id=waz_biens.bi_id AND waz_photos.bi_id=waz_biens.bi_id");
 
-// Récupération des résultats    
-$aListe = $results->result();   
+// Récupération des résultats
+        $DetailsAnnonce = $Resultat1->result();
 
-// Ajoute des résultats de la requête au tableau des variables à transmettre à la vue   
-$aView["details"] = $aListe;
+// Ajoute des résultats de la requête au tableau des variables à transmettre à la vue
+        $aView["details"] = $DetailsAnnonce;
 
-$this->load->view('Headerview');
-$this->load->view('DetailsView', $aView);
+///Requete pour connaitre les options de l'annonce
+        // Exécute la requête
+        $Resultat2 = $this->db->query("SELECT waz_biens.bi_id,opt_libelle
+FROM waz_biens,waz_options, waz_composer,waz_annonces
+WHERE waz_annonces.an_id='$an_id' AND waz_biens.bi_id=waz_annonces.bi_id
+AND waz_biens.bi_id=waz_composer.bi_id AND waz_composer.opt_id=waz_options.opt_id
+");
+
+// Récupération des résultats
+        $OptionsAnnonces = $Resultat2->result();
+
+// Ajoute des résultats de la requête au tableau des variables à transmettre à la vue
+        $aView["Options"] = $OptionsAnnonces;
+
+        if (empty($OptionsAnnonces)) {$AucuneOptions = true;} else { $AucuneOptions = false;}
+
+        $aView["AucuneOptions"] = $AucuneOptions;
+
+        ////Ajout d'une vue à l'annonce visitée
+        $AjoutVue = $this->db->query("SELECT an_nbre_vues
+        FROM waz_annonces
+        WHERE waz_annonces.an_id='$an_id' 
+        ");
+
+        // Récupération des résultats
+        $AjoutVue = $AjoutVue->result();
+        foreach ($AjoutVue as $row) {$NombVues = $row->an_nbre_vues;}
+        $NombVues = $NombVues + 1;
+        $data["an_nbre_vues"] = $NombVues;
+        $this->db->update("waz_annonces", $data, "an_id = $an_id");
+
+        $this->load->view('Headerview');
+        $this->load->view('DetailsView', $aView);
 
 }
 }
-?>
--- CodeIgniter/application/controllers/AuthentificationController.php
+++ CodeIgniter/application/controllers/AuthentificationController.php
@@ -2,303 +2,270 @@
 
 //connexion et inscription + mdp oublié
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class AuthentificationController extends CI_Controller {
+class AuthentificationController extends CI_Controller
+{
 
-public function login()
-{if(!isset($this->session->login)){
-//afficher aide au debug
-$this->output->enable_profiler(TRUE);
+        public function login()
+        {if (!isset($this->session->login)) {
+                //afficher aide au debug
+                $this->output->enable_profiler(true);
 
-// Chargement des assistants 'form' et 'url'
-$this->load->helper('form', 'url'); 
+                // Chargement des assistants 'form' et 'url'
+                $this->load->helper('form', 'url');
 
-// Chargement de la librairie 'database'
-$this->load->database(); 
+                // Chargement de la librairie 'database'
+                $this->load->database();
 
+                if ($this->input->post()) 
+                { // 2ème appel de la page: traitement du formulaire
+                        $data = $this->input->post();
 
+                        $LoginEmp = $_POST['login_name'];
+                        $MdpEmp = $_POST['password_name'];
+                        // Exécute la requête
+                        $resultsemp = $this->db->query("SELECT emp_nom,emp_prenom,emp_id
+                        FROM waz_employes
+                        WHERE emp_mail = '$LoginEmp' AND emp_mdp= '$MdpEmp'");
 
-if ($this->input->post()) 
-{ // 2ème appel de la page: traitement du formulaire
+                        // Récupération des résultats
+                        $RequeteEmploye = $resultsemp->result();
 
-$data = $this->input->post();
+                        foreach ($RequeteEmploye as $row) {$EmpPrenom = $row->emp_prenom;
+                                $EmpNom = $row->emp_nom;
+                                $EmpID = $row->emp_id;}
 
-$LoginEmp = $_POST['login_name'];
-$MdpEmp = $_POST['password_name'];
+                        $EstUnEmploye = false;
+                        if (!empty($RequeteEmploye)) {$EstUnEmploye = true;}
+                        
 
-$LoginInt = $_POST['login_name'];
-$MdpInt = $_POST['password_name'];
+                        if($EstUnEmploye == true){
+                                $this->session->set_userdata('role', "Employe");
+                                $this->session->set_userdata('login', "$LoginEmp");
+                                $this->session->set_userdata('nom', "$EmpPrenom");
+                                $this->session->set_userdata('prenom', "$EmpNom");
+                                $this->session->set_userdata('ID', "$EmpID");
 
-// Exécute la requête 
-$resultsemp = $this->db->query("SELECT emp_nom,emp_prenom,emp_id
-FROM waz_employes 
-WHERE emp_mail = '$LoginEmp' AND emp_mdp= '$MdpEmp'");
+                                $this->load->view('Headerview');
+                                $this->load->view('ConnexionReussiView');
+                        }
 
-// Récupération des résultats
-$RequeteEmploye = $resultsemp->result();
+                        else{
+                        
+                        $LoginInt = $_POST['login_name'];
+                        $MdpInt = $_POST['password_name'];
 
-foreach ($RequeteEmploye as $row) { $EmpPrenom = $row->emp_prenom; $EmpNom = $row->emp_nom; $EmpID = $row->emp_id; }
+                        // Exécute la requête
+                        $resultsint = $this->db->query("SELECT in_nom,in_prenom,in_id
+                        FROM waz_internautes
+                        WHERE in_email = '$LoginInt' AND in_mdp= '$MdpInt'");
 
-// Exécute la requête 
-$resultsint = $this->db->query("SELECT in_nom,in_prenom,in_id 
-FROM waz_internautes 
-WHERE in_email = '$LoginInt' AND in_mdp= '$MdpInt'");
+                        // Récupération des résultats
+                        $RequeteInternaute = $resultsint->result();
 
-// Récupération des résultats
-$RequeteInternaute = $resultsint->result();
+                        foreach ($RequeteInternaute as $row) {$IntPrenom = $row->in_prenom;
+                                $IntNom = $row->in_nom;
+                                $IntID = $row->in_id;}
 
-foreach ($RequeteInternaute as $row) { $IntPrenom = $row->in_prenom; $IntNom = $row->in_nom; $IntID = $row->in_id; }
+                        $EstUninternaute = false;
+                        
+                        if (!empty($RequeteInternaute)) {$EstUninternaute = true;}
+                                if($EstUninternaute){
+                                $this->session->set_userdata('role', "Internaute");
+                                $this->session->set_userdata('login', "$LoginInt");
+                                $this->session->set_userdata('nom', "$IntPrenom");
+                                $this->session->set_userdata('prenom', "$IntNom");
+                                $this->session->set_userdata('ID', "$IntID");
+                                
+                                $this->load->view('Headerview');
+                                $this->load->view('ConnexionReussiView');
+                                }
 
+                                else{
+                                echo "<script type='text/javascript'>
+                                window.alert('Connexion refusé')
+                                </script>";
 
-$EstUnEmploye = false;
-$EstUninternaute = false;
+                                $this->load->view('Headerview');
+                                $this->load->view('ConnexionView');
+                                }
+                } 
+                } 
+                else 
+                { // 1er appel de la page: affichage du formulaire
+                $this->load->view('Headerview');
+                $this->load->view('ConnexionView');
+                }} 
 
-if(!empty($RequeteEmploye)){$EstUnEmploye = true;}
-if(!empty($RequeteInternaute)){$EstUninternaute = true;}
-
-
-if (empty($RequeteEmploye) && empty($RequeteInternaute)) {
-
-echo"<script type='text/javascript'>
-window.alert('Connexion refusé')
-</script>";
-
-$this->load->view('Headerview');
-$this->load->view('ConnexionView');
-} 
-
-else {
-//$this-> load->library('session');
-
-if($EstUnEmploye == true){
-$this->session->set_userdata('role', "Employe");
-$this->session->set_userdata('login', "$LoginEmp");
-$this->session->set_userdata('nom', "$EmpPrenom");
-$this->session->set_userdata('prenom', "$EmpNom");
-$this->session->set_userdata('ID', "$EmpID");
-}
-
-if($EstUninternaute == true){
-$this->session->set_userdata('role', "Internaute");
-$this->session->set_userdata('login', "$LoginInt");
-$this->session->set_userdata('nom', "$IntPrenom");
-$this->session->set_userdata('prenom', "$IntNom");
-$this->session->set_userdata('ID', "$IntID");
-}
-
-
-$this->load->view('Headerview');
-$this->load->view('ConnexionReussiView');
-}
-
-
-
-
-} 
-
-else 
-{ // 1er appel de la page: affichage du formulaire
-$this->load->view('Headerview');
-$this->load->view('ConnexionView');
-}}
-else {
-$Erreur = "Vous êtes déjà connectés !";
-// Ajoute des résultats de la requête au tableau des variables à transmettre à la vue   
-$aView["RefusAcces"] = $Erreur;
-
-$this->load->view('Headerview',$aView);
-}}
-
+        else {
+        $Erreur = "Vous êtes déjà connecté(e) !";
+        // Ajoute des résultats de la requête au tableau des variables à transmettre à la vue
+        $aView["RefusAcces"] = $Erreur;
+        $this->load->view('Headerview', $aView);
+        }
+        }
 
 
 
 
 public function Deconnexion()
 {
-if(isset($this->session->login)){
-session_destroy();
-$this->load->helper('url');
-redirect(site_url("AccueilController/Accueil"));}
-else {
-$Erreur = "Vous n'êtes pas connecté !";
-// Ajoute des résultats de la requête au tableau des variables à transmettre à la vue   
-$aView["RefusAcces"] = $Erreur;
+        if (isset($this->session->login)) {
+        session_destroy();
+        $this->load->helper('url');
+        redirect(site_url("AccueilController/Accueil"));} else {
+        $Erreur = "Vous n'êtes pas connecté(e) !";
+        // Ajoute des résultats de la requête au tableau des variables à transmettre à la vue
+        $aView["RefusAcces"] = $Erreur;
 
-$this->load->view('Headerview',$aView);
+        $this->load->view('Headerview', $aView);
+        }
 }
-}
+
+
 
 
 public function DetailsCompte()
-{if(isset($this->session->login)){
+{if (isset($this->session->login)) {
 // $IDcompte = L'itrnasmis par la connexion
 
-
-
 //afficher aide au debug
-$this->output->enable_profiler(TRUE);
+        $this->output->enable_profiler(true);
 
 // Charge la librairie 'database'
-$this->load->database();
+        $this->load->database();
 
-if($this->session->role == "Internaute"){
-$Login = $this->session->login;
-// Exécute la requête 
-$DetailsInternaute = $this->db->query("SELECT * 
+        if ($this->session->role == "Internaute") {
+        $Login = $this->session->login;
+// Exécute la requête
+            $DetailsInternaute = $this->db->query("SELECT *
 FROM waz_internautes
-WHERE in_email='$Login'");  
+WHERE in_email='$Login'");
 
-$Details = $DetailsInternaute->result();   
+        $Details = $DetailsInternaute->result();
 
-// Ajoute des résultats de la requête au tableau des variables à transmettre à la vue   
-$aView["Details"] = $Details;
+// Ajoute des résultats de la requête au tableau des variables à transmettre à la vue
+        $aView["Details"] = $Details;
 
-// Appel de la vue avec transmission du tableau  
-$this->load->view('Headerview');
-$this->load->view('DetailsCompteView', $aView);
-}
-
-else if($this->session->role == "Employe")
-{
-$Login = $this->session->login;
-// Exécute la requête 
-$DetailsEmploye = $this->db->query("SELECT * 
+// Appel de la vue avec transmission du tableau
+        $this->load->view('Headerview');
+        $this->load->view('DetailsCompteView', $aView);
+        } else if ($this->session->role == "Employe") {
+        $Login = $this->session->login;
+// Exécute la requête
+            $DetailsEmploye = $this->db->query("SELECT *
 FROM waz_employes
-WHERE emp_mail='$Login'");  
+WHERE emp_mail='$Login'");
 
-$Details = $DetailsEmploye->result();   
+        $Details = $DetailsEmploye->result();
 
-// Ajoute des résultats de la requête au tableau des variables à transmettre à la vue   
-$aView["Details"] = $Details;
+// Ajoute des résultats de la requête au tableau des variables à transmettre à la vue
+        $aView["Details"] = $Details;
 
-// Appel de la vue avec transmission du tableau  
-$this->load->view('Headerview');
-$this->load->view('DetailsCompteView', $aView);
-}
-else{
+// Appel de la vue avec transmission du tableau
+        $this->load->view('Headerview');
+        $this->load->view('DetailsCompteView', $aView);
+        } else {
         $this->load->helper('url');
-        redirect(site_url("AccueilController/Accueil")); 
-}
-}
-else {
-$Erreur = "Vous devez être connecté pour avoir accés à cette page !";
-// Ajoute des résultats de la requête au tableau des variables à transmettre à la vue   
-$aView["RefusAcces"] = $Erreur;
+        redirect(site_url("AccueilController/Accueil"));
+        }
+} else {
+        $Erreur = "Vous devez être connecté(e) pour avoir accés à cette page !";
+// Ajoute des résultats de la requête au tableau des variables à transmettre à la vue
+        $aView["RefusAcces"] = $Erreur;
 
-$this->load->view('Headerview',$aView);
+        $this->load->view('Headerview', $aView);
 }}
 
 
 
-public function Inscription()
 
-{if(!isset($this->session->login)){
+public function Inscription()
+{if (!isset($this->session->login)) {
 // Chargement des assistants 'form' et 'url'
-$this->load->helper('form', 'url'); 
+        $this->load->helper('form', 'url');
 
 // Chargement de la librairie 'database'
-$this->load->database(); 
+        $this->load->database();
 
 // Chargement de la librairie form_validation
-$this->load->library('form_validation'); 
+        $this->load->library('form_validation');
 
-
-
-if ($this->input->post()) 
-   { // 2ème appel de la page: traitement du formulaire
+        if ($this->input->post()) { // 2ème appel de la page: traitement du formulaire
 
         $data = $this->input->post();
 
-        
-         // Définition des filtres, ici une valeur doit avoir été saisie pour le champ 'pro_ref'
-        
-        
-        $config = array(
-                array(
-                        'field' => 'in_nom',
-                        'label' => 'Nom',
-                        'rules' => 'required'
-                )
-                );
-        
-        
-        
-        $this->form_validation->set_rules($config);
-        $this->form_validation->set_rules("in_prenom", "prénom", "required|max_length[15]", array("required" => "Le %s doit être obligatoire.","max_length" => "Le %s doit avoir une longueur maximum de 15 caractères !"));
-        $this->form_validation->set_rules("in_telephone", "Téléphone", array('required', 'min_length[10]', 'max_length[10]', 'callback_tel_check')); 
-        $this->form_validation->set_rules('in_email', 'Email', 'trim|required|valid_email',); 
-        
-        $config = array(
-                array(
-                        'field' => 'in_adresse',
-                        'label' => 'Adresse',
-                        'rules' => 'required' 
-                )
-                );
+            // Définition des filtres, ici une valeur doit avoir été saisie pour le champ 'pro_ref'
 
-                                
+        $config = array(
+                array(
+                'field' => 'in_nom',
+                'label' => 'Nom',
+                'rules' => 'required',
+                ),
+        );
+
+        $this->form_validation->set_rules($config);
+        $this->form_validation->set_rules("in_prenom", "prénom", "required|max_length[15]", array("required" => "Le %s doit être obligatoire.", "max_length" => "Le %s doit avoir une longueur maximum de 15 caractères !"));
+        $this->form_validation->set_rules("in_telephone", "Téléphone", array('required', 'min_length[10]', 'max_length[10]', 'callback_tel_check'));
+        $this->form_validation->set_rules('in_email', 'Email', 'trim|required|valid_email', );
+
+        $config = array(
+                array(
+                'field' => 'in_adresse',
+                'label' => 'Adresse',
+                'rules' => 'required',
+                ),
+        );
+
         $mail = $_POST['in_email'];
         $city = $_POST['in_pays'];
 
-        $results = $this->db->query("SELECT in_id 
-        FROM waz_internautes 
+        $results = $this->db->query("SELECT in_id
+        FROM waz_internautes
         WHERE in_email = '$mail'");
 
+        $test = $results->result();
+        if (empty($test)) {
 
-                $test = $results->result();
-                if (empty($test)) {
-        
-        
-                        $this->db->insert('waz_internautes', $data);
-        
-        $this->load->view('congratz');
-        } else {
-
-
-                echo"<script type='text/javascript'>
-                window.alert('Le mail que vous avez choisi est déjà utilisé')
-                </script>";     
-
-
-        }       
-
-        
-        
-        if ($this->form_validation->run() == FALSE)
-        { // Echec de la validation, on réaffiche la vue formulaire 
-
-        $this->load->view('Inscriptionview');
-        }
-        else
-        { // La validation a réussi, nos valeurs sont bonnes, on peut insérer en base
-
-                
                 $this->db->insert('waz_internautes', $data);
 
-                echo"<script type='text/javascript'>
-                window.alert('Votre compte a été crée !')
-                </script>";  
-                $this->load->helper('url');
-                redirect(site_url("AccueilController/Accueil"));   
+                $this->load->view('congratz');
+        } else {
 
-        } 
+                echo "<script type='text/javascript'>
+                window.alert('Le mail que vous avez choisi est déjà utilisé')
+                </script>";
+
         }
-        else 
-           { // 1er appel de la page: affichage du formulaire
+
+            if ($this->form_validation->run() == false) { // Echec de la validation, on réaffiche la vue formulaire
+
+                $this->load->view('Inscriptionview');
+            } else { // La validation a réussi, nos valeurs sont bonnes, on peut insérer en base
+
+                $this->db->insert('waz_internautes', $data);
+
+                echo "<script type='text/javascript'>
+                window.alert('Votre compte a été crée !')
+                </script>";
+                $this->load->helper('url');
+                redirect(site_url("AccueilController/Accueil"));
+
+        }
+        } else { // 1er appel de la page: affichage du formulaire
         $this->load->view('Headerview');
         $this->load->view('Inscriptionview');
 
         }
-        
 
-}
-else {
-$Erreur = "Vous êtes déjà connecté !";
-// Ajoute des résultats de la requête au tableau des variables à transmettre à la vue   
-$aView["RefusAcces"] = $Erreur;
+} else {
+        $Erreur = "Vous êtes déjà connecté !";
+// Ajoute des résultats de la requête au tableau des variables à transmettre à la vue
+        $aView["RefusAcces"] = $Erreur;
 
-$this->load->view('Headerview',$aView);
+        $this->load->view('Headerview', $aView);
 }
 }
 
