df8888296bcb10da0f5a6b2a41e2914ed752ea26
Dos Santos Vincent <vincedos@hotmail.com>
2021-06-04 20:13:54 +0200

    Commit PHP
    Maintenant lorsque les annonces sont affichés le prix est precisé si par mois ou total selon le type d'offre(vente=prix total/loca=prix par mois)(changement fait que pour le fichier ventesview qui devrait regrouper toutes les annonces lundi,
    Sur la page qui affiche les annonces on voit plus l'id de l'annonce mais sa localisation,
    Page détails de l'annonce : si pas connecté en tant qu'internaute message précisant qu'il faut se connecter pour contacter l'entreprise,
    Maintenant seul les annonces active sont affichés,
    Page tous les commentaires refaite,
    Format date changé pour les commentaires (de:Année/Mois/Jour/Heure/Minutes a:Jour/Mois/Année/Heure/Minutes),
    + d'autres détails,

--- CodeIgniter/application/views/CommentairesView.php
+++ CodeIgniter/application/views/CommentairesView.php
@@ -1,40 +1,62 @@
+<head>
+        <meta charset="utf-8">
+        <title>Wazaa Immo</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" href="<?= base_url("assets/CSS/formsstyle.css") ?>">
+</head>
+<br>
+<div class='row'>
+
 <form action="" method="GET">
-    <input type="submit" name="Trie" value="NoteCroissante">
-    <input type="submit" name="Trie" value="NoteDecroissante">
+    <button type="submit" class='btn btn-outline-danger' name="Trie" value="NoteCroissante"> Note Croissante</button>
+    <button type="submit" class='btn btn-outline-danger' name="Trie" value="NoteDecroissante"> Note Decroissante</button>
 </form> 
 
 <?php 
     $url=site_url("MembresController/Commentaires"); 
     echo "<form action='$url' method='post'>";
 ?>
-    <input type="submit" value="Date croissante">
+    <button type="submit" class='btn btn-outline-danger' value="Date croissante"> Date croissante</button>
 </form> 
 
 <form action="" method="GET">
-    <input type="submit" name="Trie" value="DateDecroissante">
+    <button type="submit" name="Trie" class='btn btn-outline-danger' value="DateDecroissante"> Date Decroissante</button>
 </form> 
 
-<div class="row">
-    <div class="col-12">
-        <h1>Tout les commentaires</h1>
+</div>
+
+
+<div class="container col-12" id="topworst">    
+            <div class='row'>
+                <div class="col">
+                    <div class="form-heading" id="top">
+                        <span class="prg alert alert-dark font-weight-bold">Tout les commentaires</span>
+                    </div>
+                </div>
+            </div>
+            </div>
+
+
+
         <?php if(isset($_GET['Trie'])){$TypeDeTrie = $_GET['Trie'];}
 
         if(!isset($_GET['Trie']) ):        
         //Affichage de tous les commentaires de l'annonce trié par date décroissante
         foreach ($ResDateDecroissante as $data): 
         ?>
-            
-        <!-- Prénom de la personne mettant le commentaire -->
-        <p> <?php echo $data->in_prenom; ?> </p>
-        
-        <!-- Commentaire de la personne -->
-        <p> <?php echo $data->com_avis; ?> </p>
-        
-        <!-- Note de la personne -->
-        <p> <?php echo $data->com_notes; ?> </p>
-        
-        <!-- Date et heure du commentaire -->
-        <p> <?php echo $data->com_date_ajout; ?> </p>
+            <?php
+            //Prenom et note de la personne mettant le commentaire
+            echo "<ul class='list-group list-group-flush' id='tw'><li class='list-group-item list-group-item-action'> Prénom : ".$data->in_prenom."&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Note : ".$data->com_notes."/5</li>";
+
+            //Commentaire de la personne
+            echo "<li class='list-group-item list-group-item-action'>".$data->com_avis."</li>";
+
+            //Date et heure du commentaire
+            $Date = $data->com_date_ajout;
+            $Date = date("d-m-Y h:i", strtotime($Date));
+            echo "<li class='list-group-item list-group-item-action'>".$Date."</li></ul>";  
+            ?>
         <br>
         <br>
         
@@ -58,17 +80,18 @@
         foreach ($ResNoteCroissante as $data): 
         ?>
             
-        <!-- Prénom de la personne mettant le commentaire -->
-        <p> <?php echo $data->in_prenom; ?> </p>
-        
-        <!-- Commentaire de la personne -->
-        <p> <?php echo $data->com_avis; ?> </p>
-        
-        <!-- Note de la personne -->
-        <p> <?php echo $data->com_notes; ?> </p>
-        
-        <!-- Date et heure du commentaire -->
-        <p> <?php echo $data->com_date_ajout; ?> </p>
+            <?php
+            //Prenom et note de la personne mettant le commentaire
+            echo "<ul class='list-group list-group-flush' id='tw'><li class='list-group-item list-group-item-action'> Prénom : ".$data->in_prenom."&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Note : ".$data->com_notes."/5</li>";
+
+            //Commentaire de la personne
+            echo "<li class='list-group-item list-group-item-action'>".$data->com_avis."</li>";
+
+            //Date et heure du commentaire
+            $Date = $data->com_date_ajout;
+            $Date = date("d-m-Y h:i", strtotime($Date));
+            echo "<li class='list-group-item list-group-item-action'>".$Date."</li></ul>";  
+            ?>
         <br>
         <br>
         
@@ -91,17 +114,18 @@
         foreach ($ResNoteDecroissante as $data): 
         ?>
             
-        <!-- Prénom de la personne mettant le commentaire -->
-        <p> <?php echo $data->in_prenom; ?> </p>
-        
-        <!-- Commentaire de la personne -->
-        <p> <?php echo $data->com_avis; ?> </p>
-        
-        <!-- Note de la personne -->
-        <p> <?php echo $data->com_notes; ?> </p>
-        
-        <!-- Date et heure du commentaire -->
-        <p> <?php echo $data->com_date_ajout; ?> </p>
+            <?php
+            //Prenom et note de la personne mettant le commentaire
+            echo "<ul class='list-group list-group-flush' id='tw'><li class='list-group-item list-group-item-action'> Prénom : ".$data->in_prenom."&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Note : ".$data->com_notes."/5</li>";
+
+            //Commentaire de la personne
+            echo "<li class='list-group-item list-group-item-action'>".$data->com_avis."</li>";
+
+            //Date et heure du commentaire
+            $Date = $data->com_date_ajout;
+            $Date = date("d-m-Y h:i", strtotime($Date));
+            echo "<li class='list-group-item list-group-item-action'>".$Date."</li></ul>";  
+            ?>
         <br>
         <br>
         
@@ -124,17 +148,18 @@
         foreach ($ResDateCroissante as $data): 
         ?>
             
-        <!-- Prénom de la personne mettant le commentaire -->
-        <p> <?php echo $data->in_prenom; ?> </p>
-        
-        <!-- Commentaire de la personne -->
-        <p> <?php echo $data->com_avis; ?> </p>
-        
-        <!-- Note de la personne -->
-        <p> <?php echo $data->com_notes; ?> </p>
-        
-        <!-- Date et heure du commentaire -->
-        <p> <?php echo $data->com_date_ajout; ?> </p>
+            <?php
+            //Prenom et note de la personne mettant le commentaire
+            echo "<ul class='list-group list-group-flush' id='tw'><li class='list-group-item list-group-item-action'> Prénom : ".$data->in_prenom."&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Note : ".$data->com_notes."/5</li>";
+
+            //Commentaire de la personne
+            echo "<li class='list-group-item list-group-item-action'>".$data->com_avis."</li>";
+
+            //Date et heure du commentaire
+            $Date = $data->com_date_ajout;
+            $Date = date("d-m-Y h:i", strtotime($Date));
+            echo "<li class='list-group-item list-group-item-action'>".$Date."</li></ul>";  
+            ?>
         <br>
         <br>
         
